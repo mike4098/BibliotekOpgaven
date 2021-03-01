e41373ed98a0f5ae64a244a044dc74e3fdf27182
mike4098 <69966597+mike4098@users.noreply.github.com>
2021-03-01 08:57:26 +0100

    Add project files.

---
+++ Bibloteket/Book.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteket
+{
+    class Book
+    {
+        public string name;
+        public int sides;
+        public bool hardcover;
+        public string category;
+
+        private string Name
+        {
+            set
+            {
+                this.name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+        private int Sides
+        {
+            set
+            {
+                this.sides = value;
+            }
+            get
+            {
+                return sides;
+            }
+        }
+        private bool Hardcover
+        {
+            set
+            {
+                this.hardcover = value;
+            }
+            get
+            {
+                return hardcover;
+            }
+        }
+        private string Category
+        {
+            set
+            {
+                this.category = value;
+            }
+            get
+            {
+                return category;
+            }
+        }
+        public Book()
+        {
+
+        }
+        public Book(string inputname, int inputside, bool inputhardcover, string inputcategory)
+        {
+            this.name = inputname;
+            this.sides = inputside;
+            this.hardcover = inputhardcover;
+            this.category = inputcategory;
+        }
+        
+        
+        // A list of books in the library
+        static List<Book> books = new List<Book>();
+
+       
+        // Adds all of the books
+        public void addbooks()
+        {
+            books.Add(new Book("Bog1", 200, true, "Crime"));
+            books.Add(new Book("Bog2", 20, false, "Disney"));
+            books.Add(new Book("Bog3", 100, true, "Crime"));
+            books.Add(new Book("Cisco Network", 600, true, "Boring"));
+            books.Add(new Book("Bog5", 90, true, "Crime"));
+            books.Add(new Book("Bog6", 180, true, "True-Crime"));
+            books.Add(new Book("Bog7", 150, true, "Crime"));
+            books.Add(new Book("Bog8", 10, true, "Disney"));
+            books.Add(new Book("Bog9", 50, true, "Disney"));
+            books.Add(new Book("Bog10", 250, true, "True-Crime"));
+
+        }
+
+        
+        // Method to see all the books on the shelfs
+        public void seeall()
+        {
+            foreach (var item in books)
+            {
+                Console.WriteLine("Navn: " + item.name + " Antal sider: " + item.sides + " Hårdt omslag: " + item.hardcover + " Kategori: " + item.category);
+            }
+        }
+
+    
+        // Rents all the books 
+        public Book Lendbooks(string findnameinput)
+        {
+            foreach (var item in books)
+            {
+                if (item.name == findnameinput)
+                {
+                    books.Remove(item);
+                    return item;
+                }
+
+            }
+            return null;
+        }
+    }
+}
---
+++ Bibloteket/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Makes a new list "Shopping Cart"
+            List<Book> usersStack = new List<Book>();
+
+           
+            // Places a bunch of books on the counter
+            Stack<string> usersbooks = new Stack<string>();
+            Book logic = new Book();
+
+            
+            // Adds all books
+            logic.addbooks();
+            while (true)
+            {
+                Console.WriteLine("[1]find bøger [2]Lån bøger [3]Se resten af bøgerne ");
+                string userinput = Console.ReadLine();
+                switch (userinput)
+                {
+                    case "1":
+                        logic.seeall();
+                        Console.WriteLine("Skriv navnet på den bog du vil låne");
+                        string inputname = Console.ReadLine();
+
+                        //Asking the method to find the book you're searching for
+                        Book LendBookToUser = logic.Lendbooks(inputname);
+                        if (LendBookToUser != null)
+                        {
+                          
+                            //adds to the shopping cart
+                            usersStack.Add(LendBookToUser);
+                        }
+                        break;
+                    case "2":
+
+                        // Moves all the books from the shopping cart to the counter
+                        movebook();
+                        //  prints the name of the book for everytime there is an element in the queue
+                        for (int i = 0; i <= usersbooks.Count; i++)
+                        {
+                            Console.WriteLine("Næste bog til at scanne hedder: " + usersbooks.Peek());
+
+                         
+                            // Here its scanned and removed from the queue
+                            usersbooks.Pop(); 
+
+                        }
+
+
+                        break;
+                    case "3":
+
+                      
+                        // Method to see the rest of the books
+                        logic.seeall();
+                        break;
+                    default:
+                        break;
+                }
+
+
+
+                Console.ReadLine();
+            }
+            // Method to move the books from the shopping cart to the count
+            void movebook()
+            {
+
+                foreach (var item in usersStack)
+                {
+                    // Adds the name of the list to the queue
+                    usersbooks.Push(item.name);
+                }
+                //Removes everything from the shopping cart
+                usersStack.Clear();
+
+
+
+            }
+        }
+    }
+}
